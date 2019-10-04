b0b37b14fc59caae54e4ccc98eb5a9d833692820
JUNIOR BATISTA <JUNIORAP0101@GMAIL.COM>
2019-10-03 22:27:29 -0300

    create person seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create([{
+                 name: 'Johnny Cash',
+                 document: '555555555',
+                 date_of_birth: '26/02/1932'.to_date
+               },
+               {
+                 name: 'Sid Vicious',
+                 document: '555555555',
+                 date_of_birth: '10/05/1957'.to_date
+               },
+               {
+                 name: 'Axl Rose',
+                 document: '555555555',
+                 date_of_birth: '06/02/1962'.to_date
+               },
+               {
+                 name: 'Joey Ramone',
+                 document: '555555555',
+                 date_of_birth: '19/05/1951'.to_date
+               },
+               {
+                 name: 'Bruce Dickinson',
+                 document: '555555555',
+                 date_of_birth: '07/08/1958'.to_date
+               },
+               {
+                 name: 'Kurt Cobain',
+                 document: '555555555',
+                 date_of_birth: '20/02/1967'.to_date
+               },
+               {
+                 name: 'Elvis Presley',
+                 document: '555555555',
+                 date_of_birth: '17/08/2008'.to_date
+                }])

5905048abae39bb069db5a2ea8563f0d97b1970b
JUNIOR BATISTA <JUNIORAP0101@GMAIL.COM>
2019-10-03 23:09:16 -0300

    add pet kind seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,3 +44,13 @@ people = [{
 people.each do |person|
   Person.where(person).first_or_create
 end
+
+pet_kinds = [{ name: 'Cavalo' },
+             { name: 'Cachorro' },
+             { name: 'Papagaio' },
+             { name: 'Lhama' },
+             { name: 'Iguana' },
+             { name: 'Ornitorrinco' }]
+pet_kinds.each do |pet_kind|
+  PetKind.where(pet_kind).first_or_create
+end

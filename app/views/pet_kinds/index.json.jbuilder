e55657742c3193ee65b21ee31324c68d236dc3b6
JUNIOR BATISTA <JUNIORAP0101@GMAIL.COM>
2019-10-09 18:54:14 -0300

    add petkind scaffold controller and views

---
+++ app/views/pet_kinds/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @pet_kinds, partial: "pet_kinds/pet_kind", as: :pet_kind
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pet_kinds
   root to: "pets#index"
   resources :pets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
